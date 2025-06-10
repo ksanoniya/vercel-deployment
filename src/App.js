ee32957935b36a8f4ef1465d4b25a9ef45dd499d
kalash <kalashsanoniya1818@gmail.com>
2025-06-10 16:08:09 +0530

    add aap.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
-
+// src/App.js
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <h1>Hello from Vercel Deployment Practice 🚀</h1>;
 }
-
-export default App;
